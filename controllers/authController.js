d21ae5262813b14a477886de48f9fed6f24a5b93
Himanshusaini12 <himanshusaini962001@gmail.com>
2023-02-12 06:51:57 +0000

    JWT token added

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,13 +1,16 @@
 const User = require("./../modals/userModel");
-
+const JWT =require('jsonwebtoken')
 exports.signup =async(req,res,next)=>{
     
  try{ const newUser =await User.create(req.body)
-    
+       
+       const token=JWT.sign({id:newUser._id},process.env.JWT_SECRET)
+      
     res.status(201).json({
         status:'success',
         data:{
-            user:newUser
+            user:newUser,
+            token_d:token
         }
     })}
     catch(err){
